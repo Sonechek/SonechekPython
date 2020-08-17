3d8efeb740a6626e6d656a4da8329bb2064f14a9
Sonechek <flex.tlex@gmail.com>
2020-08-18 01:30:05 +0500

    master

--- Labrab02/05.py
+++ Labrab02/05.py
@@ -4,7 +4,7 @@ n = int(input())
 sum_1 = 0
 for i in range(n):
     if i % 2 == 0:
-        sum_1 = sum_1 +(x**(n+2))/(math.factorial(n+2))
-    else:
         sum_1 = sum_1 + (x**(n+2))/(math.factorial(n+2))
+    else:
+        sum_1 = sum_1 - (x**(n+2))/(math.factorial(n+2))
 print("%.3f" % sum_1 )
