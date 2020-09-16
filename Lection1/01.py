3d4c2bd79f9841482ab77c7a153dc529efc032c7
Sonechek <flex.tlex@gmail.com>
2020-09-16 20:25:24 +0500

    main

---
+++ Lection1/01.py
@@ -0,0 +1,11 @@
+import math
+
+
+def get_result(radius):
+    result = radius * 2 * math.pi # формула
+    return result
+
+radius = float(input("Введите радиус - "))
+print(get_result (radius))
+# input()
+# repo/labrab_01/task_05.py
