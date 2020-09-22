569368e32d651ec8a352b132f212dd79bdcf1d41
Sonechek <flex.tlex@gmail.com>
2020-09-23 01:17:35 +0500

    master

--- Labrab01/02.py
+++ Labrab01/02.py
@@ -1,8 +1,12 @@
 num_1 = int(input('Введите первое число - '))
 num_2 = int(input('Введите второе число - '))
 
-num_max = num_1
-num_min = num_2
+if num_1 > num_2:
+    num_max = num_1
+    num_min = num_2
+else:
+    num_max = num_2
+    num_min = num_1
 
 if (num_max % num_min == 0):
     print(num_max, 'кратно', num_min)
