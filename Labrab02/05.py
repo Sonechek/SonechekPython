ac696c3a8bec4eaaa67899c7ece9226f6fa2b1b5
Sonechek <flex.tlex@gmail.com>
2020-09-29 20:26:15 +0500

    master

--- Labrab02/05.py
+++ Labrab02/05.py
@@ -1,2 +1,10 @@
+import math
 x = int(input())
-q = float(input())
+n = int(input())
+math.sin(x) = x
+for i in range(n):
+    if i % 2 == 0:
+        math.sin(x) += (x**(n+2)/(math.factorial(n+2))
+    else:
+        math.sin(x) -= (x**(n+2)/(math.factorial(n+2))
+print("%.3f"(math.sin(x))
