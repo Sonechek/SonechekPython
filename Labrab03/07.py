f9cb2030c26a7a831f01aab57caba47975abc23a
Sonechek <flex.tlex@gmail.com>
2020-10-01 19:57:22 +0500

    master

--- Labrab03/07.py
+++ Labrab03/07.py
@@ -1,8 +1,8 @@
 n = int(input())
 a = str('X')
 b = str('-')
-for i in range(n):
-    if i % 2 == 1:
-        print(n*(a+b)[:-1])
+for i in range(n+2):
+    if i % 2 == 0:
+        print((n*(a+b))[:-1])
     else:
-        print(n*(b+a)[:-1])
+        print((n*(b+a))[:-1])
