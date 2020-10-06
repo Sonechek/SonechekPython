2066f815a6ec3761ba2cf4f060d7fa9fe6aefbed
Sonechek <71063702+Sonechek@users.noreply.github.com>
2020-10-06 20:06:44 +0500

    master

--- SportProg/statistika.py
+++ SportProg/statistika.py
@@ -1,2 +1,13 @@
 n = int(input())
-b = map()
+lst = [int(input()) for i in range(n)]
+for i in range(n):
+    if lst[i] % 2 == 0:
+        lst_4 = [lst[i]]
+    else:
+        lst_3 = [lst[i]]
+print(lst_3)
+print(lst_4)
+if len(lst_4) > len(lst_3):
+    print('Yes')
+else:
+    print('No')
