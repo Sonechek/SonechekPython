e919bc48baf6a84a834282f706dbfaf3017e60fd
Sonechek <flex.tlex@gmail.com>
2020-10-07 20:39:08 +0500

    master

--- Labrab04/03.py
+++ Labrab04/03.py
@@ -1,4 +1,10 @@
 f1 = open('/home/sanek/Рабочий стол/input.txt', 'r')
 text  = f1.read()
-print(text)
+lines = text.split('\n')
+n = len(lines)
+f2 = open('/home/sanek/Рабочий стол/output.txt', 'w')
+for i in range(n):
+    line = str(i+1) + ' ' + lines[i] + '\n'
+    f2.write(line)
 f1.close()
+f2.close()
