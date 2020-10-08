38ef10188ea4ac3fd4deced79e4bd81d693bfde5
Sonechek <flex.tlex@gmail.com>
2020-10-08 17:10:47 +0500

    master

--- Labrab04/05.py
+++ Labrab04/05.py
@@ -2,15 +2,9 @@ f1 = open('/home/sanek/Рабочий стол/input.txt', 'r')
 text  = f1.read()
 lines = text.split('\n')
 acc = 0
-n = len(lines)
-
-print(len(lines))
-for i in range(n):
-    if sum(list(map(int, lines[i].split(' ')))) > sum(list(map(int, lines[i+1].split(' ')))):
-        acc = lines[i]
-    else:
-        acc = lines[i+1]
-
-
+n = len(lines) 
+for i in range(n-1):
+        if sum(list(map(int, lines[i].split(' ')))) > sum(list(map(int, lines[i+1].split(' ')))):
+            acc = lines[i]
 print(acc)
 f1.close()
