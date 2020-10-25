30f8934ad152f2dca97cf926dd4404bc238664e3
Sonechek <flex.tlex@gmail.com>
2020-10-26 03:15:59 +0500

    master

--- Labrab04/06.py
+++ Labrab04/06.py
@@ -1,6 +1,17 @@
 f1 = open('/home/sanek/Рабочий стол/input.txt', 'r')
 text  = f1.read()
+line = text.split('\n')
+lines = text.split()
+lines_vt = lines[::-1]
+n = len(line)
+acc = 0
+bat = 0
+for i in range (n):
+    acc +=                      #Здесь мы будем добавлять i-ый элемент n-ой строки в аккумилирующую переменную
+    bat +=                      #А здесь мы будем добавлять i[::-1]-ый элемент n[::-1]-ой строки в акк переменную
+    i += n
 
-
-
+# print(lines)
+# print(lines_vt)
+print(acc, bat)
 f1.close()  
