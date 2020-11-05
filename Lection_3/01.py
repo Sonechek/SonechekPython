66a1bfff849c0b5d87acec57780c627162ad1fe1
Sonechek <71063702+Sonechek@users.noreply.github.com>
2020-11-05 12:35:16 +0500

    master

--- Lection_3/01.py
+++ Lection_3/01.py
@@ -1,3 +1,10 @@
 import requests
 
 link = 'https://pcoding.ru/csv/11.txt'
+
+s = requests.get(link).content.decode('utf-8')
+lst = s.split(' ')
+acc = 0
+for item in lst:
+    acc += int(item)
+print(acc)
---
+++ Lection_3/02.py
@@ -0,0 +1,5 @@
+import requests
+
+link = 'https://pcoding.ru/csv/11.txt'
+
+s = requests.get(link).content.decode('utf-8')
