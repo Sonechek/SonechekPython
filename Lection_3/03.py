4be049e70c9deb67ddd206dcd457dc7e40d4651d
Sonechek <71063702+Sonechek@users.noreply.github.com>
2020-11-05 12:53:36 +0500

    master

--- Lection_3/03.py
+++ Lection_3/03.py
@@ -2,3 +2,9 @@ import requests
 
 link = 'https://pcoding.ru/csv/11.txt'
 s = requests.get(link).content.decode('utf-8')
+s = list(map(int, s.split()))
+lst = []
+for item in s:
+    if item % 2 != 0:
+        lst.append(item)
+print(lst)
