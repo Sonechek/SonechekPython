52615a3924632fd1f99ff698555a529e64d825f6
Sonechek <flex.tlex@gmail.com>
2020-12-01 18:47:31 +0500

    master

--- Labrab06/03.py
+++ Labrab06/03.py
@@ -1,5 +1,8 @@
 f1 = open('/home/sanek/Рабочий стол/input.txt')
 text = f1.read()
-a = list(text.split())
-print(a)
+a = list(text.split()
+summa = int(a[0])
+while a[i+2] != a[n]:
+    summa = summa + a[]
+
 f1.close()
--- Labrab06/04.py
+++ Labrab06/04.py
@@ -1,3 +1,13 @@
-n = int(input())
-for item in len(n):
-    
+n = list(input())
+summa_1 = 0
+summa_2 = 0
+for i in range(len(n)):
+    if n[i] == '0':
+        summa_1 += 1
+    else: 
+        if summa_1 > summa_2:
+            summa_2 = summa_1
+        summa_1 = 0
+        
+        
+print(summa_2)
