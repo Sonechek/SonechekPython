47e0a0412dd9716223ff1c9211b7ab21fabfc2d0
Sonechek <flex.tlex@gmail.com>
2021-03-24 02:12:22 +0500

    master

--- Labs/Semestr_2/Labrab5/gradient.py
+++ Labs/Semestr_2/Labrab5/gradient.py
@@ -1,6 +1,6 @@
 from PIL import Image
 
-img = Image.open(r'''c:\Users\alexa\OneDrive\Desktop\SonechekPython\Labs\Semestr_2\Labrab3\1.jpg''')
+img = Image.open(r'''/home/sonechek/Рабочий стол/SonechekPython/Labs/Semestr_2/Labrab5/1.jpg''')
 
 width, height = img.size
 
