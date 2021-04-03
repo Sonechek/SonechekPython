0413a882ce74764cdcfbd82c09c4b5eb038d3c73
Sonechek <71063702+Sonechek@users.noreply.github.com>
2021-04-03 18:21:16 +0500

    master

--- Labs/Semestr_2/Labrab6/sifr/sifr_func.py
+++ Labs/Semestr_2/Labrab6/sifr/sifr_func.py
@@ -3,12 +3,12 @@
 def code(alph, key):
     with open(f'input.txt', mode='r', encoding='utf8') as o:
         tabs = o.read().split('\n')
-    with open(f'output.txt', mode='w', encoding='utf8') as inp:
+    with open(f'output.txt', mode='w', encoding='utf8') as w:
         for i in range(len(tabs)):
             line = ''
             for j in range(len(tabs[i])):
                 line += alph[(alph.index(tabs[i][j]) + key) % len(alph)]
-            inp.write(line + '\n')
+            w.write(line + '\n')
 
 
 rus_alph = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ1234567890'
