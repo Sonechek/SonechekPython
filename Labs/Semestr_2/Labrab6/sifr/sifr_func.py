edfb911b9b127b8110523b79a508cccc91462ca9
Sonechek <71063702+Sonechek@users.noreply.github.com>
2021-04-03 18:19:07 +0500

    master

--- Labs/Semestr_2/Labrab6/sifr/sifr_func.py
+++ Labs/Semestr_2/Labrab6/sifr/sifr_func.py
@@ -1,20 +1,20 @@
-def code(alph, line, key):
-    result = ''
-    result = ''.join([chr(ord(smb)+ chr(ord((aplh)))*key) for smb in line])
+# from PIL import Image
+
+def code(alph, key):
+    with open(f'input.txt', mode='r', encoding='utf8') as o:
+        tabs = o.read().split('\n')
+    with open(f'output.txt', mode='w', encoding='utf8') as inp:
+        for i in range(len(tabs)):
+            line = ''
+            for j in range(len(tabs[i])):
+                line += alph[(alph.index(tabs[i][j]) + key) % len(alph)]
+            inp.write(line + '\n')
 
 
-    return result
+rus_alph = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ1234567890'
+eng_alph = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+symb = ':; !?&-+=()*/.,'
+alph = rus_alph + eng_alph + symb
+key = 3
 
-
-
-rus = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
-nums = '1234567890'
-smbs = ':; !&-+=()*/.,'
-alph = rus + rus.upper() + nums + smbs
-
-line = ''
-line = input()
-key = 9412
-
-line_code = code(alph, line, key)
-print(line_code)
+code(alph, key)
